f8a4c4780c163281f4bd09066ed956e855d6faa6
anyway89 <yoru.ayan>
2025-11-18 20:51:51 +0530

    Python project

---
+++ main.py
@@ -0,0 +1,14 @@
+list = (1,1,'hello',11,21)
+a = list.index(1)
+
+print(a)
+
+
+
+# a = (1,4,2,5,6, "rohan", False)# imutabble
+# no = a.index(2)
+# print(no)
+b = 'here are some of the great things  ,'
+
+
+print(b.replace('  ,','.'))
